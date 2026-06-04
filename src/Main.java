a9922beb2187891fc9fd1c6e5f175415433cfac6
prasanth-vaindam <vaindam.prasanth@gmail.com>
2026-06-04 19:50:14 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+
+	void display() {
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Students");
+		System.out.println("Welcome To Java Class DayOne");
+		System.out.println("How are you?");
+
+	}
+
+}
