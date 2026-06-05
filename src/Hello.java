146c46d1c6efa874c3a91a137174465038587ff8
prasanth-vaindam <vaindam.prasanth@gmail.com>
2026-06-05 19:30:45 +0530

    day 02 after class

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.print("One");
+        System.out.print("Two");
+        System.out.println("3");
+        System.out.println("4");
+
+    }
+}
--- src/Variables.java
+++ src/Variables.java
@@ -1,5 +1,13 @@
 public class Variables {
     public static void main(String[] args) {
-        int age;
+        byte age = 17;
+        short marks = 500;
+        char grade = 'A';
+
+        System.out.println("age of the student " + age);
+        System.out.println("Grade of the student " + grade);
+        System.out.println("Marks of the student " + marks);
+
+
     }
 }
