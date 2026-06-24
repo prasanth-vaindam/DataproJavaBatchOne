66c649dc31f97257b290735d41c8e37f5ce13328
prasanth-vaindam <vaindam.prasanth@gmail.com>
2026-06-24 14:39:37 +0530

    methods

---
+++ src/D0016/Converter.java
@@ -0,0 +1,12 @@
+package D0016;
+
+class Converter {
+
+    static double kmToMiles(double km) {
+        return km * 0.621371;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Converter.kmToMiles(10));
+    }
+}
